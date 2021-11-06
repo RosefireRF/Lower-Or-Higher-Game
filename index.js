c1967f40a9e62c43c5e4c98f718d0f04dfa0d135
Appleiris <57938200+Appleiris@users.noreply.github.com>
2021-11-06 18:35:05 +0100

    Version 0.0.2 (Working)
    Works as intended, could use a visual update

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 let numberToGuess = Math.floor(Math.random() * 100);
-console.log(numberToGuess);
 
 function guessNumber(){
-    let holder = document.getElementById('shower');
+    let holder = document.getElementById('red');
+    let holderHolder = document.getElementById('shower');
     guessedNumber = document.getElementById("numberHolder").value;
-    holder.className = "";
+    holderHolder.removeChild(holder);
+    holderHolder.appendChild(holder);
     document.documentElement.style.setProperty('--red', 0 + "%")
-    document.documentElement.style.setProperty('--green', 100 + "%")
     if (guessedNumber == numberToGuess){
         console.log("Congratulations!");
     }
-     //Treba da ide na više (Crveno od dole na gore)
+    //Treba da ide na više (Crveno od dole na gore)
     else if (guessedNumber < numberToGuess){
-        holder.className = "bottomTo";
-        document.documentElement.style.setProperty('--green', 100 - guessedNumber + "%")
+        holder.classList.add("botTo");
+        document.documentElement.style.setProperty('--red', guessedNumber + "%")
         console.log("Go higher!");
     }
     //Treba da ide niže (Crveno da ide na dole)
     else if (guessedNumber > numberToGuess){
-        holder.className = "topTo";
+        holder.classList.add("topTo");
         document.documentElement.style.setProperty('--red', 100 - guessedNumber + "%")
         console.log("Go lower");
     }
